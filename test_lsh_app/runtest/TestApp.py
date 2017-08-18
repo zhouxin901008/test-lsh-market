6b90d5f70b7287cc51b09715029af6f4f6d5a293
zhouxin901008 <zhouxin@lsh123.com>
2017-08-18 11:52:25 +0800

    update

--- test_lsh_app/runtest/TestApp.py
+++ test_lsh_app/runtest/TestApp.py
@@ -3,7 +3,7 @@ import os
 import sys
 import unittest
 
-sys.path.append(os.path.dirname(os.getcwd()))
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 from test_lsh_app.base.AppBase import AppBase
 from test_lsh_app.base.AppRunSystemEnv import AppRunSystemEnv
 from test_lsh_app.modules.login.LoginTest import LoginTest
